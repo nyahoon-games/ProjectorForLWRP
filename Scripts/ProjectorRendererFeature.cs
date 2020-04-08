dc241cf681e7076f57fccb64a04370ef7d6cd478
Takao Kato <takao@nyahoon.com>
2020-04-08 15:40:50 +0800

    fixed a possible issue where by projectors will not work

--- Scripts/ProjectorRendererFeature.cs
+++ Scripts/ProjectorRendererFeature.cs
@@ -14,45 +14,93 @@ namespace ProjectorForLWRP
 {
 	public class ProjectorRendererFeature : ScriptableRendererFeature
 	{
-		private static ProjectorRendererFeature s_instance;
-		private Dictionary<Camera, RenderProjectorPass> m_projectorPasses = new Dictionary<Camera, RenderProjectorPass>();
+		private static ProjectorRendererFeature s_currentInstance = null;
+		private static int s_instanceCount = 0;
+		private static Dictionary<Camera, RenderProjectorPass> s_projectorPasses = null;
 		public static void AddProjector(ProjectorForLWRP projector, Camera camera)
 		{
-			if (s_instance == null)
+#if UNITY_EDITOR
+			// check if the current Forward Renderer has the ProjectorRendererFeature instance.
+			LightweightRenderPipelineAsset renderPipelineAsset = UnityEngine.Rendering.LWRP.LightweightRenderPipeline.asset;
+			if (renderPipelineAsset == null)
+			{
+				return;
+			}
+			UnityEditor.SerializedObject serializedObject = new UnityEditor.SerializedObject(renderPipelineAsset);
+			UnityEditor.SerializedProperty rendererDataProperty = serializedObject.FindProperty("m_RendererData");
+			UnityEngine.Rendering.LWRP.ScriptableRendererData rendererData = rendererDataProperty.objectReferenceValue as UnityEngine.Rendering.LWRP.ScriptableRendererData;
+			if (rendererData == null)
+			{
+				Debug.LogError("The current Lightweight Render Pipeline Asset does not have Forward Renderer Data! Please set a Forward Renderer Data which contains ProjectorRendererFeature to the current render pipeline asset.", renderPipelineAsset);
+			}
+			else
+			{
+				bool found = false;
+				foreach (var rendererFeature in rendererData.rendererFeatures)
+				{
+					if (rendererFeature is ProjectorRendererFeature)
+					{
+						found = true;
+						break;
+					}
+				}
+				if (!found)
+				{
+					Debug.LogError("ProjectorRendererFeature is not added to the current Forward Renderer Data.", rendererData);
+				}
+			}
+#endif
+			if (s_projectorPasses == null)
 			{
 #if UNITY_EDITOR
-				Debug.LogError("No ProjectorRendererFeature in the ForwardRendererData!");
+				Debug.LogError("No ProjectorRendererFeature instances are created!");
 #endif
 				return;
 			}
-			s_instance.AddProjectorInternal(projector, camera);
+			AddProjectorInternal(projector, camera);
 		}
-		public static bool checkUnityProjectorComponentEnabled { get { return s_instance != null && s_instance.m_checkUnityProjectorComponentEnabled; } }
+		public static bool checkUnityProjectorComponentEnabled { get { return s_currentInstance == null || s_currentInstance.m_checkUnityProjectorComponentEnabled; } }
 		public bool m_checkUnityProjectorComponentEnabled = true;
 		public ProjectorRendererFeature()
 		{
-			m_projectorPasses = new Dictionary<Camera, RenderProjectorPass>();
-			s_instance = this;
+			if (s_projectorPasses == null)
+			{
+				s_projectorPasses = new Dictionary<Camera, RenderProjectorPass>();
+			}
+			++s_instanceCount;
+			s_currentInstance = this;
+		}
+		~ProjectorRendererFeature()
+		{
+			if (--s_instanceCount == 0)
+			{
+				s_projectorPasses = null;
+			}
+			if (s_currentInstance == this)
+			{
+				s_currentInstance = null;
+			}
 		}
 		public override void Create()
 		{
 		}
 		public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
 		{
+			s_currentInstance = this;
 			RenderProjectorPass pass;
-			if (m_projectorPasses.TryGetValue(renderingData.cameraData.camera, out pass))
+			if (s_projectorPasses.TryGetValue(renderingData.cameraData.camera, out pass))
 			{
 				renderer.EnqueuePass(pass);
 			}
 		}
-		private void AddProjectorInternal(ProjectorForLWRP projector, Camera camera)
+		private static void AddProjectorInternal(ProjectorForLWRP projector, Camera camera)
 		{
 			RenderProjectorPass pass;
-			if (!m_projectorPasses.TryGetValue(camera, out pass))
+			if (!s_projectorPasses.TryGetValue(camera, out pass))
 			{
 				pass = new RenderProjectorPass(camera);
 				pass.renderPassEvent = projector.renderPassEvent;
-				m_projectorPasses.Add(camera, pass);
+				s_projectorPasses.Add(camera, pass);
 			}
 			pass.AddProjector(projector);
 		}
