1df76e85f21fed9a0412da3a7b7b13fdf0ede1a8
Takao Kato <takao@nyahoon.com>
2021-09-20 13:43:48 +0800

    add render pipeline asset check in projector editor

--- Scripts/Editor/ProjectorEditor.cs
+++ Scripts/Editor/ProjectorEditor.cs
@@ -8,6 +8,7 @@
 
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Rendering.Universal;
 
 namespace ProjectorForLWRP
 {
@@ -16,7 +17,30 @@ namespace ProjectorForLWRP
 	{
 		private SerializedProperty m_stencilPassProperty;
 		private SerializedProperty m_stencilOptionProperty;
+		private SerializedObject m_pipelineAssetObject;
+		private ScriptableRendererData m_rendererData;
 		private Projector m_projectorComponent;
+		private GUIStyle m_errorTextStyle;
+		private GUIStyle errorTextStyle
+		{
+			get
+			{
+				if (m_errorTextStyle == null)
+				{
+					m_errorTextStyle = new GUIStyle();
+					m_errorTextStyle.richText = true;
+					m_errorTextStyle.wordWrap = true;
+					m_errorTextStyle.alignment = TextAnchor.MiddleCenter;
+					GUIStyleState textStyle = new GUIStyleState();
+					textStyle.textColor = Color.red;
+					m_errorTextStyle.normal = textStyle;
+					m_errorTextStyle.hover = textStyle;
+					m_errorTextStyle.active = textStyle;
+					m_errorTextStyle.focused = textStyle;
+				}
+				return m_errorTextStyle;
+			}
+		}
 		private void OnEnable()
 		{
 			// It is disabled to edit Projector component in Unity 2020.3 or later.
@@ -29,6 +53,30 @@ namespace ProjectorForLWRP
 
 			m_stencilOptionProperty = null;
 			m_stencilPassProperty = null;
+
+			// check render pipeline asset
+			m_pipelineAssetObject = null;
+			PrepareURPAssetSerializedProperties();
+		}
+		private void PrepareURPAssetSerializedProperties()
+		{
+			if (m_pipelineAssetObject == null)
+			{
+				UniversalRenderPipelineAsset renderPipelineAsset = UniversalRenderPipeline.asset;
+				if (renderPipelineAsset != null)
+				{
+					m_pipelineAssetObject = new SerializedObject(renderPipelineAsset);
+				}
+			}
+			if (m_pipelineAssetObject != null && m_rendererData == null)
+			{
+				SerializedProperty rendererDataListProperty = m_pipelineAssetObject.FindProperty("m_RendererDataList");
+				SerializedProperty defaultRendererIndexProperty = m_pipelineAssetObject.FindProperty("m_DefaultRendererIndex");
+				if (defaultRendererIndexProperty.intValue < rendererDataListProperty.arraySize)
+				{
+					m_rendererData = rendererDataListProperty.GetArrayElementAtIndex(defaultRendererIndexProperty.intValue).objectReferenceValue as ScriptableRendererData;
+				}
+			}
 		}
 		private void OnDisable()
 		{
@@ -103,6 +151,71 @@ namespace ProjectorForLWRP
 				}
 			}
 			serializedObject.ApplyModifiedProperties();
+			PrepareURPAssetSerializedProperties();
+			bool isGUIEnabled = GUI.enabled;
+			if (m_pipelineAssetObject == null)
+			{
+				EditorGUILayout.TextArea("Universal Render Pipeline is not used. The projector will not be rendered with the above settings.", errorTextStyle);
+			}
+			else if (m_rendererData == null)
+			{
+				EditorGUILayout.TextArea("No Forward Renderer found. Please check the Render Pipeline Asset below.", errorTextStyle);
+				GUI.enabled = false;
+				EditorGUILayout.ObjectField("Render Pipeline Asset", m_pipelineAssetObject.targetObject, typeof(UnityEngine.Rendering.RenderPipelineAsset), false);
+				GUI.enabled = isGUIEnabled;
+			}
+			else
+			{
+				bool found = false;
+				foreach (var rendererFeature in m_rendererData.rendererFeatures)
+				{
+					if (rendererFeature is ProjectorRendererFeature)
+					{
+						found = true;
+						break;
+					}
+				}
+				if (!found)
+				{
+					EditorGUILayout.TextArea("Please Add 'Projector Renderer Feature' to the default renderer (" + m_rendererData.name + ").", errorTextStyle);
+					GUI.enabled = false;
+					EditorGUILayout.ObjectField("Current Default Renderer", m_rendererData, typeof(ScriptableRendererData), false);
+					GUI.enabled = isGUIEnabled;
+					if (GUILayout.Button("Add Projector Renderer Feature to the default renderer"))
+					{
+						ProjectorRendererFeature rendererFeature = CreateInstance<ProjectorRendererFeature>();
+						rendererFeature.name = "Projector Renderer Feature";
+						Undo.RegisterCreatedObjectUndo(rendererFeature, "Add Projector Renderer Feature");
+
+						// Store this new effect as a sub-asset so we can reference it safely afterwards
+						// Only when we're not dealing with an instantiated asset
+						if (EditorUtility.IsPersistent(m_rendererData))
+						{
+							AssetDatabase.AddObjectToAsset(rendererFeature, m_rendererData);
+						}
+						AssetDatabase.TryGetGUIDAndLocalFileIdentifier(rendererFeature, out var guid, out long localId);
+
+						SerializedObject rendererDataSerializedObject = new SerializedObject(m_rendererData);
+						SerializedProperty rendererFeaturesProperty = rendererDataSerializedObject.FindProperty("m_RendererFeatures");
+						SerializedProperty rendererFeaturesMapProperty = rendererDataSerializedObject.FindProperty("m_RendererFeatureMap");
+						// Grow the list first, then add - that's how serialized lists work in Unity
+						rendererFeaturesProperty.arraySize++;
+						SerializedProperty componentProp = rendererFeaturesProperty.GetArrayElementAtIndex(rendererFeaturesProperty.arraySize - 1);
+						componentProp.objectReferenceValue = rendererFeature;
+
+						// Update GUID Map
+						rendererFeaturesMapProperty.arraySize++;
+						SerializedProperty guidProp = rendererFeaturesMapProperty.GetArrayElementAtIndex(rendererFeaturesMapProperty.arraySize - 1);
+						guidProp.longValue = localId;
+						rendererDataSerializedObject.ApplyModifiedProperties();
+
+						if (EditorUtility.IsPersistent(m_rendererData))
+						{
+							EditorUtility.SetDirty(m_rendererData);
+						}
+					}
+				}
+			}
 
 			// It is disabled to edit Projector component in Unity 2020.3 or later.
 			// We need to show Projector properties in this GUI.
